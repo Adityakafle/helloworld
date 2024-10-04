42f48195da4ab5a0cbf5f49ceaa54f98824dbe4c
AdityaKafle <jaishiaditya2000@gmail.com>
2024-10-04 11:59:57 +0545

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+def greet_user(name):
+    """Function to greet the user."""
+    print(f"Hello, {name}! Welcome to the Simple Greeting Application.")
+
+def main():
+    """Main function to run the application."""
+    print("Welcome to the Simple Greeting Application!")
+    user_name = input("Please enter your name: ")
+    greet_user(user_name)
+
+if __name__ == "__main__":
+    main()
