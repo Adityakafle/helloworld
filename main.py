3aaad89046308142d4ef4af460802e46963a1386
Aditya Jaishi <43834263+Adityakafle@users.noreply.github.com>
2024-10-07 13:03:34 +0545

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 def main():
     """Main function to run the application."""
-    print("Welcome . to the Simple Greeting Application!")
+    print("Welcome to the Simple Greeting Application!")
     
 
 if __name__ == "__main__":
