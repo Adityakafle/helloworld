9037f547190afb50b61c0531e1815ff04abc6314
Aditya Jaishi <43834263+Adityakafle@users.noreply.github.com>
2024-10-07 12:58:08 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-def greet_user(name):
-    """Function to greet the user."""
-    print(f"Hello, {name}! Welcome to the Simple Greeting Application.")
+
 
 def main():
     """Main function to run the application."""
     print("Welcome . to the Simple Greeting Application!")
-    user_name = input("Please enter your name: ")
-    greet_user(user_name)
+    
 
 if __name__ == "__main__":
     main()
